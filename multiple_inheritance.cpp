02e22ddf0462c53d54268048d91a86a88edd42da
Puspa Kamal oli <105839410+PuspaKamalOli@users.noreply.github.com>
2023-02-28 10:03:07 +0545

    Add files via upload

---
+++ multiple_inheritance.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+using namespace std;
+class A
+{
+
+    int a, b;
+
+public:
+    void x()
+    {
+
+        cout << "hello universe" << endl;
+    };
+};
+
+class B
+{
+
+public:
+    void y()
+    {
+        cout << "hello world";
+    };
+};
+
+class C : public A, public B
+{
+
+public:
+    void z()
+    {
+        x();
+        y();
+    };
+};
+int main()
+{
+    C obj;
+    obj.x();
+    obj.y();
+
+    return 0;
+}
